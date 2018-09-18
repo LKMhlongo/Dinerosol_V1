ec6d960317d548deb2497b7921bfcfbd0d58f0a2
Lindokuhle Mhlongo <kb.magaye@gmail.com>
2018-09-18 08:37:30 +0200

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dinerosol.Startup))]
+namespace Dinerosol
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
